aa57b37693d3c1eb72d69c2c2d724843926d6173
Tiago Moreira <tiago.joke@yahoo.com.br>
2022-02-16 17:58:51 -0300

    feat: add controller get method router task

--- src/controllers/task/router.js
+++ src/controllers/task/router.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 router.post('/', require('./create'));
+router.get('/', require('./getAll'));
 
 module.exports = router;
