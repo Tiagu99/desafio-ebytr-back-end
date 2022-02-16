4d4f35992bea30b4041b8f9949620697f062c5e4
Tiago Moreira <tiago.joke@yahoo.com.br>
2022-02-16 17:02:19 -0300

    feat: add in taskModel create function

--- src/models/entity/taskModel.js
+++ src/models/entity/taskModel.js
@@ -2,6 +2,12 @@ const connection = require('../connection');
 
 const getAll = async () => (await connection()).collection('task').find().toArray();
 
+const create = async (doc) => {
+  const { insertedId: _id } = await (await connection())
+    .collection('task').insertOne({ ...doc });
+  return { _id, ...doc };
+}
+
 module.exports = {
   getAll,
 };
