21cd671b793f5d406f1b3f09a25b688ad866bc87
Tiago Moreira <tiago.joke@yahoo.com.br>
2022-02-16 17:18:03 -0300

    feat: add controller create task

---
+++ src/controllers/task/create.js
@@ -0,0 +1,16 @@
+const service = require('../../services/task/create');
+
+module.exports = (req, res, next) => {
+  try {
+    const { task } = req.body;
+
+    if(!task) return res.status(400).send({ message: 'must be contain "task" key' });
+
+    const created = service({ task });
+
+    return res.status(201).send(created);
+
+  } catch (error) {
+    return next(error);
+  }
+}
