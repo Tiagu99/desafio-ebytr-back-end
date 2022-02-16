255e7eb7ddad4ca8aeb1f2c4b322891ea4041ecc
Tiago Moreira <tiago.joke@yahoo.com.br>
2022-02-16 16:53:48 -0300

    feat: add connection db mongoDB

---
+++ src/models/connection.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Ebytr`;
+const DB_NAME = 'Ebytr';
+
+let connection = null;
+
+module.exports = async () => {
+  try {
+    if(connection) return connection;
+    connection = (await MongoClient.connect(
+      MONGO_DB_URL,
+      {
+        useNewUrlParse: true,
+        useUnifiedTopology: true,
+      },
+    )).db(DB_NAME);
+    return connection;
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
