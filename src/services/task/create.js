075e23111bd3fd98cf9b78ef1ec350cfce54adb3
Tiago Moreira <tiago.joke@yahoo.com.br>
2022-02-17 03:39:57 -0300

    feat: asure status defaul "pendente" at tasks

--- src/services/task/create.js
+++ src/services/task/create.js
@@ -2,6 +2,6 @@ const Model = require('../../models/entity/taskModel');
 
 module.exports = async (doc) => {
   const time = Date.now();
-  const createdTask = await Model.create({ ...doc, time });
+  const createdTask = await Model.create({ ...doc, time, status: 'Pendente' });
   return createdTask;
 }
